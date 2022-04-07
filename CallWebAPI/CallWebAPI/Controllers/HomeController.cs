5b6de64b8456186efa733ae08c15e690221c7cf7
q12389 <q12389@yahoo.com.tw>
2022-04-07 22:36:38 +0800

    建立簡單的WebAPI 以及Call WebApi的網頁（需下中斷點確認取到的資料）

---
+++ CallWebAPI/CallWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallWebAPI.Controllers
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public async Task<ActionResult> Index()
+        {
+            List<Product> EmpInfo = new List<Product>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                string Baseurl = "https://localhost:44333/";
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("/api/product");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    EmpInfo = JsonConvert.DeserializeObject<List<Product>>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return View(EmpInfo);
+            }
+        }
+    }
+}
